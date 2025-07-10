c09f94dddcaee587ecf2632277e40ad62647d1cc
M0rfe00 <g.lucas24@hotmail.com>
2025-07-10 13:47:23 +0200

    Quartz sync: Jul 10, 2025, 1:47 PM

--- quartz/components/Explorer.backup.tsx
+++ quartz/components/Explorer.backup.tsx
@@ -30,7 +30,10 @@ const defaultOptions: Options = {
     return node
   },
   sortFn: (a, b) => {
+    // Sort order: folders first, then files. Sort folders and files alphabeticall
     if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
+      // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
+      // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
       return a.displayName.localeCompare(b.displayName, undefined, {
         numeric: true,
         sensitivity: "base",
@@ -91,37 +94,31 @@ export default ((userOpts?: Partial<Options>) => {
             <line x1="4" x2="20" y1="18" y2="18" />
           </svg>
         </button>
-
-        {/* Solo se muestra el título si opts.title tiene contenido */}
-        {opts.title ? (
-          <button
-            type="button"
-            class="title-button explorer-toggle desktop-explorer"
-            data-mobile={false}
-            aria-expanded={true}
+        <button
+          type="button"
+          class="title-button explorer-toggle desktop-explorer"
+          data-mobile={false}
+          aria-expanded={true}
+        >
+          <h2>{opts.title ?? i18n(cfg.locale).components.explorer.title}</h2>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            viewBox="5 8 14 8"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="fold"
           >
-            <h2>{opts.title}</h2>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="14"
-              height="14"
-              viewBox="5 8 14 8"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="fold"
-            >
-              <polyline points="6 9 12 15 18 9"></polyline>
-            </svg>
-          </button>
-        ) : null}
-
+            <polyline points="6 9 12 15 18 9"></polyline>
+          </svg>
+        </button>
         <div class="explorer-content" aria-expanded={false}>
           <OverflowList class="explorer-ul" />
         </div>
-
         <template id="template-file">
           <li>
             <a href="#"></a>
