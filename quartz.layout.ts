f6a8d43d238b9cb7c34a62e17faf0b7838e00bcb
M0rfe00 <g.lucas24@hotmail.com>
2026-01-19 23:29:19 +0100

    Quartz sync: Jan 19, 2026, 11:29 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -92,7 +92,7 @@ export const defaultContentPageLayout: PageLayout = {
         drag: true,
         zoom: true,
         depth: -1,
-        scale: 3,
+        scale: -1,
         repelForce: 0.5, // how much nodes should repel each other
         centerForce: 1.5, // how much force to use when trying to center the nodes
         removeTags: ["hideInExplorer"],
